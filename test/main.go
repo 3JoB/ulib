13daff3aaabed59e401102db702e5c77fb9f96b1
JNyaa <125870426+JNyaa@users.noreply.github.com>
2023-02-20 17:55:24 +0800

    test: add select

--- test/main.go
+++ test/main.go
@@ -7,10 +7,16 @@ import (
 	"github.com/3JoB/ulib/fsutil/compress"
 )
 
-func main(){
-	data := fsutil.ReadPath("testdata")
-	if err := compress.NewZip().Create("test.zst.zip", data); err != nil {
-		panic(err)
+var Mode string
+
+func main() {
+	if Mode == "true" {
+		compress.NewZip().Extract("test.zst.zip", "testios")
+	} else {
+		data := fsutil.ReadPath("testdata")
+		if err := compress.NewZip().Create("test.zst.zip", data); err != nil {
+			panic(err)
+		}
+		fmt.Println("ok!")
 	}
-	fmt.Println("ok!")
 }
