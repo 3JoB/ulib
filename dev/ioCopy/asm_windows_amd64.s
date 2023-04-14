bc60f96aef409847ae86b7e2006b782f93c6fde8
Zero <54469706+3JoB@users.noreply.github.com>
2023-04-14 23:50:29 +0800

    add

--- dev/ioCopy/asm_windows_amd64.s
+++ dev/ioCopy/asm_windows_amd64.s
@@ -4,29 +4,29 @@
 #include "funcdata.h" 
 
 // func asmCopy(hSrc syscall.Handle, hDst syscall.Handle, n *int) error
-TEXT ·asmCopy(SB), NOSPLIT, $0-24
-    MOVQ  hSrc+0(FP), SI
-    MOVQ  hDst+8(FP), DI
-    MOVQ  $buf+16(FP), DX
+TEXT ·asmCopy(SB), NOSPLIT, $0-32 
+    MOVQ  hSrc+0(FP), SI      // src Handle
+    MOVQ  hDst+8(FP), DI     // dst Handle
+    MOVQ  $buf+16(FP), DX    // buffer
     MOVQ  $0, CX
-    MOVQ  CX, n+16(FP)
-
+    MOVQ  CX, n+16(FP)       // written bytes
+    
 loop: 
-    MOVL  $0, AX
+    MOVL  $0, AX           // read syscall
     SYSCALL
     
-    CMOVQ AX, CX
-    CMPQ  CX, $0
-    JE  done
+    CMOVQ AX, CX            // bytes read
+    CMPQ  CX, $0            // eof?
+    JE   done
     
-    MOVL  $1, AX
+    MOVL  $1, AX            // write syscall
     SYSCALL
     
-    ADDQ  CX, n+16(FP)
-    CMPQ  CX, DX
-    JNE  loop
-
+    ADDQ  CX, n+16(FP)      // update written bytes
+    CMPQ  CX, DX            // buffer full?
+    JNE  loop               // continue
+    
 done:
-    MOVQ  n+16(FP), AX
-    MOVQ  $0, err+24(FP)
-    RET
+    MOVQ  n+16(FP), AX      // written bytes
+    MOVQ  $0, err+24(FP)    // nil error
+ret
