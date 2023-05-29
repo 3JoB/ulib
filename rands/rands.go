48f08bef03aa414bf8f5963ed54928e68d6ce812
Zero <54469706+3JoB@users.noreply.github.com>
2023-05-29 18:05:09 +0800

    add

--- rands/rands.go
+++ rands/rands.go
@@ -9,9 +9,8 @@ func Rands[T any](n []T, num int) []T {
 	if len(n) < 1 && len(n) <= num {
 		return nil
 	}
-	idxs := rand.Perm(len(n))[:num]
 	result := make([]T, 0, num)
-	for _, idx := range idxs {
+	for _, idx := range rand.Perm(len(n))[:num] {
 		result = append(result, n[idx])
 	}
 	return result
@@ -21,9 +20,8 @@ func CRands[T any](n []T, num int) []T {
 	if len(n) < 1 && len(n) <= num {
 		return nil
 	}
-	idxs := frand.Perm(len(n))[:num]
 	result := make([]T, 0, num)
-	for _, idx := range idxs {
+	for _, idx := range frand.Perm(len(n))[:num] {
 		result = append(result, n[idx])
 	}
 	return result
